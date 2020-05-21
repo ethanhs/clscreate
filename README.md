abe19fd50357810efc2eac173372a7a5c6c04aa5
Ethan Smith <ethan@ethanhs.me>
2020-05-20 22:59:18 -0700

    Add README and Python comparison

---
+++ README.md
@@ -0,0 +1,3 @@
+# clscreate, benchmarking class creation time for fun and profit
+
+See for more https://github.com/PyO3/pyo3/issues/661
--- test_clscreate.py
+++ test_clscreate.py
@@ -5,6 +5,11 @@ import pytest
 from clscreate_cython import Example as Example_cy
 from clscreate_rs import Example as Example_rs
 
+class Example:
+    def __init__(self):
+        self.a = 4
+        self.b = "Test"
+
 @pytest.mark.benchmark(
     timer=time.perf_counter, disable_gc=True, warmup=False,
 )
@@ -17,3 +22,8 @@ def test_Example_rust(benchmark):
 def test_Example_cython(benchmark):
     benchmark(Example_cy)
 
+@pytest.mark.benchmark(
+    timer=time.perf_counter, disable_gc=True, warmup=False,
+)
+def test_Example_python(benchmark):
+    benchmark(Example)
